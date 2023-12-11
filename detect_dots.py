5f857fb8f007df13362383202e712d4b039b1bd7
Isabella <isabella.bermonr@udea.edu.co>
2023-12-11 10:17:40 -0500

    Añadir función de ángulo

--- detect_dots.py
+++ detect_dots.py
@@ -1,122 +1,125 @@
 import cv2
 import numpy as np
 
-# Leer la imagen
-image = cv2.imread('Robot.jpg')
+def calculateAngle(filepath):
+    # Leer la imagen
+    image = cv2.imread(filepath)
 
-# Establecer los valores HSV mínimo y máximo que se mostrarán
-lower = np.array([166, 66, 149])
-upper = np.array([179, 255, 255])
+    # Establecer los valores HSV mínimo y máximo que se mostrarán
+    lower = np.array([166, 66, 149])
+    upper = np.array([179, 255, 255])
 
-# Convertir la imagen a formato HSV
-hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # Convertir la imagen a formato HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-# Aplicar la máscara de color
-mask = cv2.inRange(hsv, lower, upper)
-result = cv2.bitwise_and(image, image, mask=mask)
-# cv2.imshow(result)
+    # Aplicar la máscara de color
+    mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(image, image, mask=mask)
+    # cv2.imshow(result)
 
-# Convertir la imagen con máscara a blanco y negro
-gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+    # Convertir la imagen con máscara a blanco y negro
+    gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
 
-# Aplicar umbral para resaltar objetos
-_, threshold = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+    # Aplicar umbral para resaltar objetos
+    _, threshold = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
 
-# Encontrar contornos en la imagen umbral
-contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Encontrar contornos en la imagen umbral
+    contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Dibujar contornos y encontrar centros
-image_with_contours = image.copy()
-centroids = []
-for contour in contours:
-    # Calcular el centroide del contorno
-    M = cv2.moments(contour)
-    if M["m00"] != 0:
-        cx = int(M["m10"] / M["m00"])
-        cy = int(M["m01"] / M["m00"])
+    # Dibujar contornos y encontrar centros
+    image_with_contours = image.copy()
+    centroids = []
+    for contour in contours:
+        # Calcular el centroide del contorno
+        M = cv2.moments(contour)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
 
-        # Dibujar un punto en el centro
-        cv2.circle(image_with_contours, (cx, cy), 3, (0, 255, 255), -1)
+            # Dibujar un punto en el centro
+            cv2.circle(image_with_contours, (cx, cy), 3, (0, 255, 255), -1)
 
-        # Almacenar el centro en la lista de centroides
-        centroids.append((cx, cy))
+            # Almacenar el centro en la lista de centroides
+            centroids.append((cx, cy))
 
-# Calcular la distancia entre cada par de centroides
-min_distance = float('inf')
-closest_pair = None
+    # Calcular la distancia entre cada par de centroides
+    min_distance = float('inf')
+    closest_pair = None
 
-for i in range(len(centroids) - 1):
-    for j in range(i + 1, len(centroids)):
-        distance = np.linalg.norm(np.array(centroids[i]) - np.array(centroids[j]))
-        if distance < min_distance:
-            min_distance = distance
-            closest_pair = (centroids[i], centroids[j])
+    for i in range(len(centroids) - 1):
+        for j in range(i + 1, len(centroids)):
+            distance = np.linalg.norm(np.array(centroids[i]) - np.array(centroids[j]))
+            if distance < min_distance:
+                min_distance = distance
+                closest_pair = (centroids[i], centroids[j])
 
-# Dibujar una línea que conecta los centros más cercanos
-if closest_pair:
-    cv2.line(image_with_contours, closest_pair[0], closest_pair[1], (0, 0, 255), 2)
+    # Dibujar una línea que conecta los centros más cercanos
+    if closest_pair:
+        cv2.line(image_with_contours, closest_pair[0], closest_pair[1], (0, 0, 255), 2)
 
-    ### Calcular la inclinación del robot
+        ### Calcular la inclinación del robot
 
-    # Punto medio de la línea roja
-    midpoint = ((closest_pair[0][0] + closest_pair[1][0]) // 2, (closest_pair[0][1] + closest_pair[1][1]) // 2)
+        # Punto medio de la línea roja
+        midpoint = ((closest_pair[0][0] + closest_pair[1][0]) // 2, (closest_pair[0][1] + closest_pair[1][1]) // 2)
 
-    # Dibujar el punto medio
-    cv2.circle(image_with_contours, midpoint, 3, (255, 0, 0), -1)
+        # Dibujar el punto medio
+        cv2.circle(image_with_contours, midpoint, 3, (255, 0, 0), -1)
 
-    # Calcular la pendiente negativa de la línea roja
-    slope_red_line = (closest_pair[1][1] - closest_pair[0][1]) / (closest_pair[1][0] - closest_pair[0][0])
+        # Calcular la pendiente negativa de la línea roja
+        slope_red_line = (closest_pair[1][1] - closest_pair[0][1]) / (closest_pair[1][0] - closest_pair[0][0])
 
-    # Calcular el ángulo de la línea roja en radianes
-    angle_red_line = np.arctan(slope_red_line)
+        # Calcular el ángulo de la línea roja en radianes
+        angle_red_line = np.arctan(slope_red_line)
 
-    # Convertir el ángulo a grados
-    angle_red_line_deg = np.degrees(angle_red_line)
+        # Convertir el ángulo a grados
+        angle_red_line_deg = np.degrees(angle_red_line)
 
-    # Calcular la pendiente de la recta secante
-    slope_secant_line = -1 / slope_red_line
+        # Calcular la pendiente de la recta secante
+        slope_secant_line = -1 / slope_red_line
 
-    # Calcular el ángulo de la recta secante en radianes
-    angle_secant_line = np.arctan(slope_secant_line)
+        # Calcular el ángulo de la recta secante en radianes
+        angle_secant_line = np.arctan(slope_secant_line)
 
-    # Convertir el ángulo a grados
-    angle_secant_line_deg = np.degrees(angle_secant_line)
+        # Convertir el ángulo a grados
+        angle_secant_line_deg = np.degrees(angle_secant_line)
 
-    # Imprimir el ángulo de la línea roja y la recta secante
-    print(f'Ángulo de la línea roja: {angle_red_line_deg} grados')
-    print(f'Ángulo de la recta secante: {angle_secant_line_deg} grados')
+        # Imprimir el ángulo de la línea roja y la recta secante
+        # print(f'Ángulo de la línea roja: {angle_red_line_deg} grados')
+        # print(f'Ángulo de la recta secante: {angle_secant_line_deg} grados')
 
-    # Calcular y imprimir el ángulo entre la línea roja y la recta secante
-    angle_between_lines = angle_secant_line_deg - angle_red_line_deg
-    print(f'Ángulo entre la línea roja y la recta secante: {angle_between_lines} grados')
+        # Calcular y imprimir el ángulo entre la línea roja y la recta secante
+        # angle_between_lines = angle_secant_line_deg - angle_red_line_deg
+        # print(f'Ángulo entre la línea roja y la recta secante: {angle_between_lines} grados')
 
-    # Calcular la intersección y de la recta secante (y = mx + b)
-    intercept_y = midpoint[1] - slope_secant_line * midpoint[0]
+        # Calcular la intersección y de la recta secante (y = mx + b)
+        intercept_y = midpoint[1] - slope_secant_line * midpoint[0]
 
-    # Calcular los puntos extremos de la recta secante
-    x1_secant = int(midpoint[0] - 100)
-    y1_secant = int(slope_secant_line * x1_secant + intercept_y)
+        # Calcular los puntos extremos de la recta secante
+        x1_secant = int(midpoint[0] - 100)
+        y1_secant = int(slope_secant_line * x1_secant + intercept_y)
 
-    x2_secant = int(midpoint[0] + 100)
-    y2_secant = int(slope_secant_line * x2_secant + intercept_y)
+        x2_secant = int(midpoint[0] + 100)
+        y2_secant = int(slope_secant_line * x2_secant + intercept_y)
 
-    # Dibujar la recta secante
-    cv2.line(image_with_contours, (x1_secant, y1_secant), (x2_secant, y2_secant), (0, 255, 0), 2)
+        # Dibujar la recta secante
+        cv2.line(image_with_contours, (x1_secant, y1_secant), (x2_secant, y2_secant), (0, 255, 0), 2)
 
-# Dibujar contornos en la imagen original
-cv2.drawContours(image_with_contours, contours, -1, (0, 0, 0), 2)
+    # Dibujar contornos en la imagen original
+    cv2.drawContours(image_with_contours, contours, -1, (0, 0, 0), 2)
 
-# Mostrar la imagen con contornos, puntos en el centro y línea entre centros más cercanos
-cv2.imshow('Objects Detected with Line and Secant', image_with_contours)
-cv2.imwrite("output_front.jpg", image_with_contours)
+    # Mostrar la imagen con contornos, puntos en el centro y línea entre centros más cercanos
+    # cv2.imshow('Objects Detected with Line and Secant', image_with_contours)
+    cv2.imwrite("output_front.jpg", image_with_contours)
 
-# Imprimir la cantidad de objetos detectados y la distancia mínima
-print(f'Cantidad de objetos detectados: {len(contours)}')
-# print(f'Distancia mínima entre centros: {min_distance}')
+    # Imprimir la cantidad de objetos detectados y la distancia mínima
+    # print(f'Cantidad de objetos detectados: {len(contours)}')
+    # print(f'Distancia mínima entre centros: {min_distance}')
 
-# Esperar por un evento de teclado y cerrar la ventana al presionar una tecla
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # # Esperar por un evento de teclado y cerrar la ventana al presionar una tecla
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    return angle_red_line_deg
 
 # Filtro de HSV para máscara de color
 # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv/48367205#48367205
--- predict.py
+++ predict.py
@@ -6,12 +6,23 @@ https://docs.ultralytics.com/usage/cfg/#predict
 from ultralytics import YOLO
 import numpy as np
 import cv2
+from detect_dots import calculateAngle
+import os
+import shutil
+
+# Path to robot crop
+folder = "runs/detect/predict/crops/0"
 
 # Load a model
 model = YOLO('best.pt')  # load a custom model
 
+# Eliminar la carpeta "predict"
+ruta_predict = os.path.join("runs/detect/predict")
+if os.path.exists(ruta_predict):    
+    shutil.rmtree(ruta_predict)
+
 # Load an image
-image_path = './Robocup-1/test/images/WhatsApp-Image-2023-12-07-at-5-17-33-PM_jpeg.rf.00dcce2d97dcc825015367e30657b8e9.jpg'
+image_path = './Robocup-1/valid/images/WhatsApp-Image-2023-12-07-at-5-17-07-PM_jpeg.rf.4f7d25ad9dc004142e91fe61cd1ab967.jpg'
 # Draw a circle at the center of each object and add label
 image = cv2.imread(image_path)
 # Perform object detection using the model
@@ -36,8 +47,15 @@ for r in results:
         y_center = int((coord[1] + coord[3]) / 2)
 
         if c == 0:  # Assuming class 0 corresponds to "my robot"
-            angle =  1 # Run model 2 to segment front of the robot
-            extracted_data["robot"] = [x_center, y_center, angle]
+            # Lista todos los archivos en la carpeta
+            archivos = os.listdir(folder)
+            # Encuentra la imagen (suponiendo que sea una imagen con extensión jpg)
+            imagen = next((archivo for archivo in archivos if archivo.endswith(".jpg")), None)
+            if imagen:
+                ruta_completa = os.path.join(folder, imagen)
+                print(f"La imagen se encuentra en: {ruta_completa}")
+                # angle = calculateAngle() # Run model 2 to segment front of the robot
+                extracted_data["robot"] = [x_center, y_center, calculateAngle(ruta_completa)]
         elif c == 3:  # Assuming class 1 corresponds to "pelota"
             extracted_data["pelota"] = [x_center, y_center]
         else:  # Assuming other classes correspond to "objetos"
